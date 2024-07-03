fa01f0b1d7ef25c9ca27c797721ce385eb88db56
Sowdarjya <kolaysowdarjya@gmail.com>
2024-07-03 17:28:19 +0530

    added functionality

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
+const input = document.getElementById("userInput");
+const addBtn = document.getElementById("add-btn");
+const taskList = document.getElementById("taskList");
 
+addBtn.addEventListener("click", () => {
+  if (input.value === "") {
+    alert("Task cannot be empty");
+  } else {
+    let task = document.createElement("li");
+    task.innerHTML = input.value;
+    console.log(task);
+    taskList.appendChild(task);
+  }
+  input.value = "";
+});
