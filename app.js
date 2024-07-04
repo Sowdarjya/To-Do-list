4f6d961a9ee0c4ac79b133d8ddeea9830c89dd14
Sowdarjya <kolaysowdarjya@gmail.com>
2024-07-04 09:55:42 +0530

    added functionality to delete and changed styling

--- app.js
+++ app.js
@@ -6,10 +6,17 @@ addBtn.addEventListener("click", () => {
   if (input.value === "") {
     alert("Task cannot be empty");
   } else {
-    let task = document.createElement("li");
-    task.innerHTML = input.value;
+    let task = document.createElement("p");
+    task.innerHTML = `<span>${input.value}</span><i class="fa-solid fa-trash">`;
     console.log(task);
     taskList.appendChild(task);
   }
   input.value = "";
+
+  const del = document.querySelectorAll(".fa-trash");
+  del.forEach((button) => {
+    button.onclick = () => {
+      button.parentNode.remove();
+    };
+  });
 });
